0bc3bd44969829f3c4c7b316590699fb027412ed
Anthony Castreuil <anthony.castreuil@vrt.be>
2024-01-05 11:38:19 +0100

    refactor main.py

---
+++ MXFDecoder.py
@@ -0,0 +1,50 @@
+import sys
+from pathlib import Path
+from FFMPEGTools import ffprobe, parse_ffprobe_output, ffmpeg_extract_thumbnails, FFMPEGFrameData
+from SCTE_104_Tools import decode_SCTE104
+
+PADDING = 1
+
+def DecodeMXF(filename):
+    if not Path(filename).is_file():
+        print("Could not read file: " + filename)
+        exit(1)
+    ffprobe_result = ffprobe(filename)
+    injection_frame_number_list = []
+    marker_frame_number_list = []
+    if ffprobe_result.return_code == 0:
+        ffprobe_output = parse_ffprobe_output(ffprobe_result.json)
+        for scte104_packet in ffprobe_output:
+            # strip:
+            # DID Data Identifier
+            # SDID Secondary Data Identifier
+            # DBN Data Block Number
+            # DC Data Count
+            # from the packet as the next decoding step expects only the UDW
+            result = decode_SCTE104(scte104_packet.anc_data[8:])            
+            if result.as_dict["timestamp"]["time_type"] == 0:
+                # add scte transition frame, no timestamp adjustment due to immediate trigger not containing timestamp information
+                marker_frame_number_list.append(scte104_packet.pts_frame_number)
+                print ('@frame_number: {} - file timestamp: {} - utc timestamp: {}'.format(scte104_packet.pts_frame_number, scte104_packet.pts_time, scte104_packet.utc_time))
+            if result.as_dict["timestamp"]["time_type"] == 2:
+                # add announcement frame - on this frame we announced an incoming trigger
+                injection_frame_number_list.append(scte104_packet.pts_frame_number)
+                # the morpheus driver took this margin to announce the scte packet
+                driver_margin = result.get_splice_event_timestamp() - scte104_packet.utc_time
+                # add scte transition frame, the actual transition frame
+                marker_frame_number_list.append(scte104_packet.pts_frame_number + driver_margin.frames)
+                print('@frame_number: {}/(marker found at file timestamp: {}) '.format(scte104_packet.pts_frame_number, scte104_packet.pts_time))
+                print('|_> marker injection timestamp (utc): {}'.format(scte104_packet.utc_time))
+                print('|_> marker start/end timestamp (utc): {}'.format(result.get_splice_event_timestamp()))
+                print(result)
+        # output the frame thumbnails to folder named like inputfile
+        outputfolder = Path('results')
+        outputfolder = outputfolder / (Path(filename).stem)
+        outputfolder.mkdir(parents=True, exist_ok=True)
+        
+        print("Extracting frame thumbnails..")
+        ffmpeg_result = ffmpeg_extract_thumbnails(filename, marker_frame_number_list, PADDING, outputfolder)
+    else:
+        print("ERROR while reading {}".format(filename))
+        print(ffprobe_result.error, file=sys.stderr)
+        print(ffmpeg_result.error, file=sys.stderr)
---
+++ PhabrixDecoder.py
@@ -0,0 +1,69 @@
+import logging
+import datetime
+from datetime import timezone
+from time import sleep
+
+from PhabrixTools import get_anc_data, phabrix_preprocessor
+from SCTE_104_Tools import decode_SCTE104_to_output
+
+###########
+IP = b'10.240.36.70'
+PORT = 2100
+
+MSG_PRESS_KEY = 4
+MSG_SET_VALUE = 5
+MSG_GET_VALUE = 21
+MSG_GET_TEXT = 20
+MSG_GET_ITEM_TEXT = 25
+MSG_SET_TEXT = 6
+MSG_GET_ITEM_STRINGS = 29
+MSG_GET_ITEM_VALUES = 28
+
+COM_ANLYS_LOG_VIEW = 400
+COM_ANLYS_ANC_DATA = 603
+COM_ANLYS_INP1_STD  = 560
+COM_ANLYS_2_ANC_DATA = 8004
+COM_ANLYS_3_ANC_DATA = 8104
+###########
+
+log = logging.getLogger(__name__)
+
+def DecodeANC(oneshot):
+    dateTag = datetime.datetime.now().strftime("%Y-%b-%d_%H-%M-%S")
+    #logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    logging.basicConfig(filename='scte.log_%s' % dateTag, encoding='utf-8', format='%(message)s', level=logging.INFO)
+
+    if oneshot == True:
+        anc_data = get_anc_data(IP, PORT, MSG_GET_ITEM_VALUES, COM_ANLYS_ANC_DATA)
+        data = phabrix_preprocessor(anc_data)
+        decode_SCTE104_to_output(data)
+    else:
+        try:
+            tmp_line = ""
+            while(1):    
+                anc_data = get_anc_data(IP, PORT, MSG_GET_ITEM_VALUES, COM_ANLYS_ANC_DATA)
+                # phabrix preprocessor is a function to combine all necessary data processing steps to get to the relevant anc data for our purposes
+                data = phabrix_preprocessor(anc_data)
+                                    
+                timestamp = datetime.datetime.now(timezone.utc)
+
+                if data != tmp_line:
+                    print("@", timestamp)
+                    print("Newly fetched:", data)
+                    
+                    decode_SCTE104_to_output(data)
+                    
+                    tmp_line = data
+                else:
+                    print("@", timestamp, "Same data. Omitted.")
+
+                sleep(1)
+
+        except KeyboardInterrupt:
+            #traceback.print_exc()
+            print("Shutting down loop..")
+
+
+
+def fake_anc_decode(anc_data):
+    return phabrix_preprocessor(anc_data)
