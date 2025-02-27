646e9a3ca56ce680d4f69c3d7f365b69f1a9f05b
Anthony Castreuil <castrea@Anthonys-MacBook-Air.local>
2025-02-27 13:47:17 +0100

    refactoring to increase code readability

--- MorpheusLogDecoder.py
+++ MorpheusLogDecoder.py
@@ -1,8 +1,9 @@
+import argparse
+
 from Tools.MorpheusTools import log_filtered_kerneldiag_logs, filter_kernel_diags_on_device_and_keyword
 
-def parse_log():
-    print("parse log")
-    FILE = "KernelDiags.log.2024-01-09"
+def parse_log(filename):
+    FILE = filename
     DEVICE = "SCTE104_TLNProtocol"
     KEYWORD = "SendData"
 
@@ -11,5 +12,7 @@ def parse_log():
     log_filtered_kerneldiag_logs(filter_kernel_diags_on_device_and_keyword(file = FILE, device = DEVICE, keyword = KEYWORD), ignore_keep_alive=True)
 
 if __name__ == "__main__":
-    print("inside main")
-    parse_log()
+    cli_parser = argparse.ArgumentParser(description="Read Morpheus KernelDiags log and parse SCTE104 Messages")
+    cli_parser.add_argument("filename", help="Path to the KernelDiags log file")
+    args = cli_parser.parse_args()
+    parse_log(args.filename)
--- Tools/MorpheusTools.py
+++ Tools/MorpheusTools.py
@@ -5,7 +5,7 @@ import pytz
 import datetime
 from datetime import timezone
 from timecode import Timecode
-from Tools.SCTE_104_Tools import decode_SCTE104, decode_SCTE104_to_file
+from Tools.SCTE_104_Tools import decode_SCTE104, decode_SCTE104_to_output
 from bitstring import ReadError
 from Tools.PhabrixTools import fake_anc_decode
 
@@ -15,9 +15,9 @@ def compose(*functions):
 
 # this is copied from the probel controller card log
     
-#probel_string = "0xff [0] 0xff [1] 0x0 [2] 0x50 [3] 0x0 [4] 0x0 [5] 0xe3 [6] 0x0 [7] 0x0 [8] 0x0 [9] 0x2 [10] 0xe [11] 0xe [12] 0x10 [13] 0x3 [14] 0x2 [15] 0x1 [16] 0x4 [17] 0x0 [18] 0x2 [19] 0x1f [20] 0x40 [21] 0x1 [22] 0xb [23] 0x0 [24] 0x36 [25] 0x0 [26] 0x0 [27] 0x0 [28] 0x0 [29] 0x0 [30] 0x0 [31] 0x1e [32] 0xf [33] 0x24 [34] 0x61 [35] 0x65 [36] 0x39 [37] 0x65 [38] 0x33 [39] 0x36 [40] 0x39 [41] 0x33 [42] 0x2d [43] 0x30 [44] 0x34 [45] 0x33 [46] 0x37 [47] 0x2d [48] 0x34 [49] 0x66 [50] 0x32 [51] 0x32 [52] 0x2d [53] 0x62 [54] 0x37 [55] 0x61 [56] 0x66 [57] 0x2d [58] 0x31 [59] 0x66 [60] 0x35 [61] 0x39 [62] 0x37 [63] 0x63 [64] 0x63 [65] 0x30 [66] 0x63 [67] 0x30 [68] 0x61 [69] 0x63 [70] 0xa [71] 0x0 [72] 0x0 [73] 0x0 [74] 0x0 [75] 0x0 [76] 0x0 [77] 0x0 [78] 0x0 [79]"
+#sent_driver_data_to_injector = "0xff [0] 0xff [1] 0x0 [2] 0x50 [3] 0x0 [4] 0x0 [5] 0xe3 [6] 0x0 [7] 0x0 [8] 0x0 [9] 0x2 [10] 0xe [11] 0xe [12] 0x10 [13] 0x3 [14] 0x2 [15] 0x1 [16] 0x4 [17] 0x0 [18] 0x2 [19] 0x1f [20] 0x40 [21] 0x1 [22] 0xb [23] 0x0 [24] 0x36 [25] 0x0 [26] 0x0 [27] 0x0 [28] 0x0 [29] 0x0 [30] 0x0 [31] 0x1e [32] 0xf [33] 0x24 [34] 0x61 [35] 0x65 [36] 0x39 [37] 0x65 [38] 0x33 [39] 0x36 [40] 0x39 [41] 0x33 [42] 0x2d [43] 0x30 [44] 0x34 [45] 0x33 [46] 0x37 [47] 0x2d [48] 0x34 [49] 0x66 [50] 0x32 [51] 0x32 [52] 0x2d [53] 0x62 [54] 0x37 [55] 0x61 [56] 0x66 [57] 0x2d [58] 0x31 [59] 0x66 [60] 0x35 [61] 0x39 [62] 0x37 [63] 0x63 [64] 0x63 [65] 0x30 [66] 0x63 [67] 0x30 [68] 0x61 [69] 0x63 [70] 0xa [71] 0x0 [72] 0x0 [73] 0x0 [74] 0x0 [75] 0x0 [76] 0x0 [77] 0x0 [78] 0x0 [79]"
 # Program Start
-#probel_string = "0xff [0] 0xff [1] 0x0 [2] 0x2c [3] 0x0 [4] 0x0 [5] 0xdb [6] 0x0 [7] 0x0 [8] 0x0 [9] 0x2 [10] 0x13 [11] 0x6 [12] 0xc [13] 0x15 [14] 0x2 [15] 0x1 [16] 0x4 [17] 0x0 [18] 0x2 [19] 0x1f [20] 0x40 [21] 0x1 [22] 0xb [23] 0x0 [24] 0x12 [25] 0x0 [26] 0x0 [27] 0x0 [28] 0x0 [29] 0x0 [30] 0x0 [31] 0x3c [32] 0xf [33] 0x0 [34] 0x10 [35] 0x0 [36] 0x0 [37] 0x0 [38] 0x0 [39] 0x0 [40] 0x0 [41] 0x0 [42] 0x0 [43]"
+#sent_driver_data_to_injector = "0xff [0] 0xff [1] 0x0 [2] 0x2c [3] 0x0 [4] 0x0 [5] 0xdb [6] 0x0 [7] 0x0 [8] 0x0 [9] 0x2 [10] 0x13 [11] 0x6 [12] 0xc [13] 0x15 [14] 0x2 [15] 0x1 [16] 0x4 [17] 0x0 [18] 0x2 [19] 0x1f [20] 0x40 [21] 0x1 [22] 0xb [23] 0x0 [24] 0x12 [25] 0x0 [26] 0x0 [27] 0x0 [28] 0x0 [29] 0x0 [30] 0x0 [31] 0x3c [32] 0xf [33] 0x0 [34] 0x10 [35] 0x0 [36] 0x0 [37] 0x0 [38] 0x0 [39] 0x0 [40] 0x0 [41] 0x0 [42] 0x0 [43]"
 # Ad start
 
 def list_to_string(my_list):
@@ -33,13 +33,13 @@ def convert_to_int(dataframe):
 
     return [(int(hex_data.zfill(2) , 16)) for hex_data in dataframe]
 
-def make_fake_phabrix_anc_data(probel_string):
-    probel_string = filter_probel_string(probel_string)
+def make_fake_phabrix_anc_data(sent_driver_data_to_injector):
+    sent_driver_data_to_injector = filter_sent_driver_data_to_injector(sent_driver_data_to_injector)
     raw_anc = ['1023', '1023', '577', '263', '557', '264', '767', '767', '512', '300', '512', '512', '731', '512', '512', '512', '258', '275', '518', '20', '524', '277', '258', '257', '260', '512', '258', '287', '320', '257', '267', '512', '530', '512', '512', '512', '512', '512', '512', '572', '40', '527', '512', '272', '512', '512', '512', '512', '512', '512', '512', '512', '415', '0', '0', '0', '0', '0', '0', '0', '0']
-    print(probel_string)
+    print(sent_driver_data_to_injector)
     print(raw_anc[6:])
-    print([hex(int(hex_data,16)).zfill(2) for hex_data in probel_string])
-    for hex_data in probel_string:
+    print([hex(int(hex_data,16)).zfill(2) for hex_data in sent_driver_data_to_injector])
+    for hex_data in sent_driver_data_to_injector:
         print(hex_data, bin(int(hex_data, 16) << 2), hex((int(hex_data,16))), (int(hex_data,16)), hex((int(hex_data,16))<< 2))
     for hex_data in raw_anc[6:]:
         print(hex_data, bin(int(hex_data)), hex(int(hex_data)), hex(int(hex_data) >> 2))
@@ -47,13 +47,18 @@ def make_fake_phabrix_anc_data(probel_string):
     print(hex(1023 >> 2) )
     
 
-    #chunks = [probel_string[i:i+chunk_length] for i in range(0, len(probel_string), chunk_length)]
+    #chunks = [sent_driver_data_to_injector[i:i+chunk_length] for i in range(0, len(sent_driver_data_to_injector), chunk_length)]
 
+'''
+The SCTE104 data that is logged is in an annoying format
+<hex byte data> [<data field number>]
+We only want the <hex byte data> so we skip all field numbers
+0xff [0] 0xff [1] 0x0 [2] etc. will become 0xff 0xff 0x0
+'''
+def filter_sent_driver_data_to_injector(sent_driver_data_to_injector):
+    return ([n[2:].zfill(2) for index, n in enumerate(sent_driver_data_to_injector.split(" ")) if not index%2])
 
-def filter_probel_string(probel_string):
-    return ([n[2:].zfill(2) for index, n in enumerate(probel_string.split(" ")) if not index%2])
-
-morpheus_preprocessor = compose(filter_probel_string, list_to_string)
+morpheus_preprocessor = compose(filter_sent_driver_data_to_injector, list_to_string)
 
 def log_filtered_kerneldiag_logs(line, ignore_keep_alive=False):
     logging.basicConfig(filename='scte_diags.log', encoding='utf-8', filemode='w', format='%(message)s', level=logging.INFO)
@@ -68,7 +73,9 @@ def log_filtered_kerneldiag_logs(line, ignore_keep_alive=False):
     timezone = pytz.timezone("Europe/Brussels")
     utc_offset = timezone.localize(naive)
     utc_adjusted_hour = (utc_offset.utcoffset().seconds//3600)
-
+    '''
+    The Morpheus Kernel Diags log lines are not formatted in a way that is easily parsed. We need to string slice and mangle the line data in a format that can be handled.
+    '''
     for result in line:
         single_log_line = {}
         '''
@@ -80,38 +87,38 @@ def log_filtered_kerneldiag_logs(line, ignore_keep_alive=False):
         4. Finally, constructing a single dict with the timecode, device, the original data for debugging purposes and the hex data send. This data is converted into a hex stream string.
         '''
         result = result.split(":")
-        
+        # skip the card pair and jump to the hours, in this example: "12"
         hours = result[0][len(result[0])-2:]
+        # the rest can be grabbed from the list
         minutes = result[1]
         seconds = result[2]
         frames = result[3]
-
+        # we reformat the fields so it's easily convertible to a Timecode object
         timecode_string = f"{hours}:{minutes}:{seconds}:{frames}"
         automation_ts = Timecode('25', timecode_string)
 
         utc_adjusted_timecode_string = f"{int(hours)+utc_adjusted_hour}:{minutes}:{seconds}:{frames}"
         utc_adjusted_automation_ts = Timecode('25', utc_adjusted_timecode_string)
-
+        # build the dict
         single_log_line["timecode"] = str(automation_ts)
         single_log_line["timecode_utc_adjusted"] = str(utc_adjusted_automation_ts)
         automation_ts.set_fractional(True)
         utc_adjusted_automation_ts.set_fractional(True)
         single_log_line["timecode_frac"] = str(automation_ts)
         single_log_line["timecode_utc_adjusted_frac"] = str(utc_adjusted_automation_ts)
-        
-
-        # only device needed
+        # only device needed from the "topic" datafield
         single_log_line["device"] = result[4].split(",")[0]
-        # sent data
-        probel_string = result[5][1:-14]
-        single_log_line["orig"] = probel_string
-        single_log_line["data"] = morpheus_preprocessor(probel_string)
+        # grab the sent SCTE104 data from the automation driver to the injector card
+        sent_driver_data_to_injector = result[5][1:-14]
+        print(sent_driver_data_to_injector)
+        single_log_line["orig"] = sent_driver_data_to_injector
+        single_log_line["data"] = morpheus_preprocessor(sent_driver_data_to_injector)
     
         
         all_log_lines.append(single_log_line)
 
     for scte_event_in_log in all_log_lines:
-        #print(scte_event_in_log)
+        print(scte_event_in_log)
         try:
             # 0003000dffffffff0000 = keep_alive message
             if ignore_keep_alive == True and "0003000dffffffff0000" in scte_event_in_log["data"]:
@@ -119,30 +126,32 @@ def log_filtered_kerneldiag_logs(line, ignore_keep_alive=False):
                     pass
             else:
                 log.info("@ %s (%s) ~ %s (%s)", scte_event_in_log["timecode"], scte_event_in_log["timecode_frac"], scte_event_in_log["timecode_utc_adjusted"], scte_event_in_log["timecode_utc_adjusted_frac"])
-                print("trying decode ", single_log_line["data"])
-                decode_SCTE104_to_file(scte_event_in_log["data"])
+                print("SCTE104 Message found in logs:", single_log_line["data"])
+                #decode_SCTE104_to_output(scte_event_in_log["data"])
         except ReadError:
             print("error decoding: ", scte_event_in_log)
 
 def filter_kernel_diags_on_device_and_keyword(file, device, keyword):
-    print(file)
-    #filtered = (line for filtered_lines in open('KernelDiags.log') if device in filtered_lines for line in filtered_lines if keyword in filtered_lines)
-    line = (line for line in open(file) if device in line )
-    filtered = (filtered for filtered in line if keyword in filtered )
-    return filtered
+    '''
+    This function will reads a KernelDiags log file and only returns log lines with the following conditions:
+    - device: only return log lines if it matches a certain device
+    - keyword: only return log lines if a certain command is seen on this log line
+    '''    
+    filtered_on_device = (line for line in open(file) if device in line )
+    return (filtered_on_keyword for filtered_on_keyword in filtered_on_device if keyword in filtered_on_keyword )
 
-def morpheus_log_parser(probel_string):
+def morpheus_log_parser(sent_driver_data_to_injector):
     #manual_anc = "ffff002c000073000200020a1f190c02010400021f40010b0012000002290000000000310000000000000000000a0104000b0000000c00000001"
     manual_anc = "ffff002c0000dd0002000209153b0402010400021f40010b0012000002290000000000310000000000000000000b0104000b0000000c00000001"
     #manual_anc = "ffff002200001b000100020c0d0e0f010101000e0100010000029a0fa007d00100011e"
     #print ("** Manually entered ANC:", manual_anc)
 
-    #make_fake_phabrix_anc_data(probel_string)
+    #make_fake_phabrix_anc_data(sent_driver_data_to_injector)
     #raw_anc = ['1023', '1023', '577', '263', '557', '264', '767', '767', '512', '300', '512', '512', '731', '512', '512', '512', '258', '275', '518', '20', '524', '277', '258', '257', '260', '512', '258', '287', '320', '257', '267', '512', '530', '512', '512', '512', '512', '512', '512', '572', '40', '527', '512', '272', '512', '512', '512', '512', '512', '512', '512', '512', '415', '0', '0', '0', '0', '0', '0', '0', '0']
     #print("** raw anc:", fake_anc_decode(raw_anc))
     decode_SCTE104(manual_anc)
     '''
-    data = morpheus_preprocessor(probel_string)
+    data = morpheus_preprocessor(sent_driver_data_to_injector)
     print("** Probel: ", data)
     
     decode_SCTE104(data)
